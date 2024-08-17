203aeec27223e5f5dde81e186263c023ac0e1945
Jakub Antos <antos_j@outlook.com>
2024-08-17 11:27:28 +0200

    chore: latex field quite good now

--- the_source/src/LatexField.js
+++ the_source/src/LatexField.js
@@ -1,17 +1,41 @@
 import Draggable from "react-draggable";
+import React, { useState } from 'react';
+import { BlockMath } from 'react-katex';
+import 'katex/dist/katex.min.css';
+import './index.css';
 
 
 function LatexField(props) {
+  const [latex, setLatex] = useState('');
+  const [renderedLatex, setRenderedLatex] = useState('');
+
+  const handleInputChange = (event) => {
+    setLatex(event.target.value);
+  };
+
+  const handleRenderClick = () => {
+    setRenderedLatex(latex);
+  };
+
   return (
     <Draggable>
     <div className="container" style={{left: props.left, top: props.top}}>
       <div className="divInput">
-        <textarea className="textarea"></textarea>
+        <textarea 
+          className="textarea"
+          rows="1"
+          onChange={handleInputChange}
+        ></textarea>
       </div>
       <div className="divCompile">
-        <button className="compileButton">Compile</button>
+        <button 
+          className="compileButton"
+          onClick={handleRenderClick}
+        >Render</button>
       </div>
-      <div className="divOutput"></div>
+      <div className="divOutput">
+        {renderedLatex && <BlockMath math={renderedLatex} />}
+      </div>
     </div></Draggable>
   );
 }
