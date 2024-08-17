305438c394bea131b4c4eb3d717e6aca158ec6b6
Jakub Antos <antos_j@outlook.com>
2024-08-17 11:54:12 +0200

    chore: change n_rows

--- the_source/src/LatexField.js
+++ the_source/src/LatexField.js
@@ -1,21 +1,42 @@
 import Draggable from "react-draggable";
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { BlockMath } from 'react-katex';
 import 'katex/dist/katex.min.css';
 import './index.css';
 
+function return_length(string) {
+    return string.length
+}
 
 function LatexField(props) {
-  const [latex, setLatex] = useState('');
   const [renderedLatex, setRenderedLatex] = useState('');
-
-  const handleInputChange = (event) => {
-    setLatex(event.target.value);
-  };
-
   const handleRenderClick = () => {
-    setRenderedLatex(latex);
+    setRenderedLatex(text);
   };
+  const ref = useRef(null);
+  const empty_textarea_width = 4
+  const [n_cols, setColCount] = useState(empty_textarea_width)
+  const [n_rows, setRowCount] = useState(1)
+  const [text, setText] = useState("");
+  const [prev_text_len, setPrevTextLen] = useState({n_letters: 0})
+  const [position, setPosition] = useState({left: props.left, top: props.top})
+  const backgroundColor = props.connect_mode ? "#cecdcd" : "white";
+
+  function updateElement(e) {
+      // look at previous n_letters, n_lines - compare with current
+      // -> change top / left position
+      const value = e.target.value
+      const input_lines = value.split("\n")
+      const n_lines = input_lines.length
+      const input_counts = input_lines.map(return_length)
+      const n_letters = Math.max.apply(Math, input_counts)
+      setColCount(n_letters + empty_textarea_width)
+      setRowCount(n_lines)
+      setText(value)
+      const delta_letters = n_letters - prev_text_len.n_letters
+      setPosition({left: position.left - 3*delta_letters, top: position.top})
+      setPrevTextLen({n_letters: n_letters})
+  }
 
   return (
     <Draggable>
@@ -23,8 +44,8 @@ function LatexField(props) {
       <div className="divInput">
         <textarea 
           className="textarea"
-          rows="1"
-          onChange={handleInputChange}
+          rows={n_rows}
+          onChange={updateElement}
         ></textarea>
       </div>
       <div className="divCompile">
